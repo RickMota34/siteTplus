e88d7d4881991fb3e2cf11907dea605c2dfaa1d9
Rick Mota <ricardorickricardosmt@gmail.com>
2021-08-15 18:25:36 -0300

    adicionei todos os arquivos e pastas

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Roupas TPlus</title>
+</head>
+<body>
+    <!--Start topo-->
+    <input type="checkbox" id="check">
+    <div class="top">
+
+        <label for="check">
+            <div class="bt-menu"><img src="assets/IMG/menu.png"></div>
+        </label>
+
+        <div class="text-logo"><H1>T Plus</H1></div>
+        <div class="login"><span>Entrar</span> <img src="assets/IMG/login.png"></div>
+
+    </div>
+    <!--End topo-->
+
+    <!--start menu lateral-->
+    <div class="mn-lat">
+        <ul class="cont-menu">
+            <li><a href="">Masculina</a></li>
+            <li><a href="">Feminina</a></li>
+            <li><a href=""> Infantil</a></li>
+        <ul>
+    </div>
+    <!--End menu lateral-->
+</body>
+</html>
